7574b0e8c0249bf0ac56a5704a8681d1a40d70ad
kazikkoduje <kazik@kazikkoduje.pl>
2021-10-22 11:07:02 +0200

    kazikkoduje

--- 1_firebase_auth/lib/firebase.ts
+++ 1_firebase_auth/lib/firebase.ts
@@ -1,21 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import firebase from 'firebase/app'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyDFn0CbHZEI63U1nQj-YQI_Z6KjpCVGh7I",
-  authDomain: "no-input-signal-322709.firebaseapp.com",
-  projectId: "no-input-signal-322709",
-  storageBucket: "no-input-signal-322709.appspot.com",
-  messagingSenderId: "932626794110",
-  appId: "1:932626794110:web:d16999d2002a747d26ba2f",
-  measurementId: "G-N81PG1YHJ6"
+export const firebaseConfig = {
+  apiKey: "AIzaSyAegSpstr2__ohGR72rHoQcJyu9M2VDBMY",
+  authDomain: "nobleprog-ecd3b.firebaseapp.com",
+  projectId: "nobleprog-ecd3b",
+  storageBucket: "nobleprog-ecd3b.appspot.com",
+  messagingSenderId: "423721358942",
+  appId: "1:423721358942:web:a66c397aa9c797584f46cf"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+
+
